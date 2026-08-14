9ff0d3e934f63a20d6a129db82c586cc00386a82
Suriya Prakash <suriyaprakash.ts@gmail.com>
2026-08-14 19:46:49 +0530

    New commit message

--- main.py
+++ main.py
@@ -7,4 +7,8 @@ app = Flask(__name__)   #Creating an instance of the Flask class.
 def index():
     return render_template("index.html")
 
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
 app.run(debug=True)
